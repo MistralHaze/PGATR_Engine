619f788aa136c6498e34e483c6b6973d9102d442
Carlos <shamanking96@hotmail.com>
2020-06-26 18:24:48 +0200

    Adapted descriptor pools and ordered commands

--- VKNgine/vulkanApp.cpp
+++ VKNgine/vulkanApp.cpp
@@ -64,15 +64,14 @@ void vulkanApp::runVulkanCompute ( )
 
   createBuffer();
 
-  //Commands
-  createCommandPool ( );
-
   //createUniformBuffer ( );
-
   createDescriptorPool ( );
   createDescriptorSet ( );
 
+  //Commands
+  createCommandPool ( );
   createCommandBuffers ( );
+
   createSemaphores ( );
 }
 
@@ -439,11 +438,11 @@ void vulkanApp::copyBufferToImage ( VkBuffer buffer,
 
 void vulkanApp::createDescriptorPool ( )
 {
-  std::array < VkDescriptorPoolSize, 2 > poolSizes = {};
-  poolSizes[0].type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+  std::array < VkDescriptorPoolSize, 1 > poolSizes = {};
+  //De momento solo guardamos guardamos descriptores de Storage
+  poolSizes[0].type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
   poolSizes[0].descriptorCount = 1;
-  poolSizes[1].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-  poolSizes[1].descriptorCount = 1;
+
 
   VkDescriptorPoolCreateInfo poolInfo = {};
   poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
